c4711c77dc876e1c1e2bac7ec275e0a993aea3d1
Artem Valikov <doksna@mail.ru>
2020-07-25 18:41:56 +0300

    Авторизация проходит успешно. Дальше будем делать регистрацию.

--- vendor/connect.php
+++ vendor/connect.php
@@ -1,4 +1,3 @@
-
 <?php
 $driver   = 'mysql';
 $host     = "localhost";
@@ -18,19 +17,8 @@ try {
     );
   // set the PDO error mode to exception
   // $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+  // echo "Connected successfully";
 } catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
-?>
-
-
-<?php
-/*
-$connect = mysqli_connect('localhost', 'root', '', 'test');
-
-if (!$connect) {
-  die('Error connect to DataBase');
-}
-*/
 ?>
--- vendor/signin.php
+++ vendor/signin.php
@@ -9,58 +9,81 @@ if (empty($_POST['login'])) {
   $_SESSION['message'] = 'Не заполнено поле "Логин"';
 }
 ##
+else {
+  require_once('connect.php');
 
-require_once ('connect.php');
+  $login = $_POST['login'];
+  // $password = md5($_POST['password']);
+  $password = $_POST['password'];
 
-$login = $_POST['login'];
-$password = md5($_POST['password']);
+  // $query=$connect->query("SELECT COUNT(*) as count FROM users");
+  // $query->setFetchMode(PDO::FETCH_ASSOC);
+  // $row=$query->fetch();
+  // $members=$row['count']; //смотрим количество строк в таблице users
 
-// $query=$connect->query("SELECT COUNT(*) as count FROM users");
-// $query->setFetchMode(PDO::FETCH_ASSOC);
-// $row=$query->fetch();
-// $members=$row['count']; //смотрим количество строк в таблице users
+  // echo $members;
+  // ----------------------------
 
-// echo $members;
-// ----------------------------
+  # 1. Текст запроса - сохраняем в переменную $sql
+  $sql = ('SELECT * FROM users WHERE (login = :login) AND (password = :password)');
 
-# 1. Текст запроса - сохраняем в переменную $sql
-$sql = ('SELECT * FROM users WHERE login = :login');
+  # 2. Подготавливаем текст запроса, с помощью функции prepare()
+  $sqlmsg = $connect->prepare($sql);
 
-# 2. Подготавливаем текст запроса, с помощью функции prepare()
-$sqlmsg = $connect->prepare($sql);
+  # 3. Указываем параметры - передаём в запрос массив с переменными
+  $data = array(
+    'login' => $login,
+    'password' => $password
+  );
+  $sqlmsg->execute($data);    //выполняем метод execute(), передав ему массив с переменными
 
-# 3. Указываем параметры - передаём в запрос массив с переменными
-$data = array (
-                'login' => $login,
-                // 'password' => $password
-);
-$sqlmsg->execute($data);    //выполняем метод execute(), передав ему массив с переменными
+  # 4. Устанавливаем режим выборки
+  $sqlmsg->setFetchMode(PDO::FETCH_ASSOC);
 
-# 4. Устанавливаем режим выборки
-$sqlmsg->setFetchMode(PDO::FETCH_ASSOC);  
+  # 5. Получаем строку
+  $row = $sqlmsg->fetch();
 
-# 5. Получаем строку
-$row = $sqlmsg->fetch();
+  // echo $row['login'];
 
-echo $row['login'];
+  // while($row = $sqlmsg->fetch()) {  
+  //     echo $row['login'] . "\n";  
+  //     echo $row['password'] . "\n";  
+  //     // echo $row['city'] . "\n";  
+  // }
 
-// while($row = $sqlmsg->fetch()) {  
-//     echo $row['login'] . "\n";  
-//     echo $row['password'] . "\n";  
-//     // echo $row['city'] . "\n";  
-// }
+  ############============---------------
 
+  #Если строка пользователя найдена - перенаправляем его на страницу профиля, и добавляем его в переменную $_SESSION['user']
+  #Иначе возвращаем в начало
+
+  if ($row) {
+
+    $user = $row;
+
+    $_SESSION['user'] = [
+      "id" => $user['id_user '],
+      "full_name" => $user['full_name'],
+      "login" => $user['login']
+    ];
+
+    header('Location: ../profile.php');
+  } else {
+    $_SESSION['message'] = 'Не верный логин или пароль';
+    header('Location: ../index.php');
+  }
+}
 ?>
 
 
 
-<pre>
+<!-- <pre>
     <?php
     // print_r($check_user);
     // print_r($user);
-    echo '---==----<br>';
-    print_r($row);
-
+    // echo '---==----<br>';
+    // print_r($row);
+    // echo '-=-<br>';
+    // echo $row['login'];
     // echo $login;
     ?>
-</pre>
+</pre> -->
