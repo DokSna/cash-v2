5c0e517f4a43395a62da1474087b67e7297f2e06
Artem Valikov <doksna@mail.ru>
2020-08-09 16:39:57 +0300

    Добавлен мета тег viewport для index

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if ($_SESSION['user']) {
 
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cash - авторизация.</title>
   <link rel="stylesheet" href="style/main.css">
 </head>
